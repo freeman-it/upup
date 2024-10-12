4192b8ac23d91aff3ce97fba39fd6710a2ed1ddf
freeman <17789620826@189.cn>
2024-10-12 15:41:57 +0800

    Revert "hell"
    This reverts commit 3a29876c2e0bbbcd8b22b373855949a32cf29fae.

--- src/main/java/com/it/reee/DockerDemoApplication.java
+++ src/main/java/com/it/reee/DockerDemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DockerDemoApplication {
 
     public static void main(String[] args) {
-        //
         SpringApplication.run(DockerDemoApplication.class, args);
     }
 
